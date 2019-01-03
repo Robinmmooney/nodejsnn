96aa62df6fb50330679976d6df155a257e7b0580
Robin Mooney <robinmooney.student@careerdevs.com>
2019-01-03 17:55:03 +0000

    updated app.js file to include finding the file and directory name using __filename and __dirname

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-var fs = require('fs');
+console.log(__dirname);
+console.log(__filename);
 
-var file = fs.readFile('input.txt','utf8',function(err,data){
-    fs.writeFile('writeme.txt',data);
-    console.log(data);
-});
 
-console.log('This is an instruction outside the sync file system.');
+
+
+// var time = 0;
+
+// var timer = setInterval(function(){
+//     time += 2;
+//   console.log(time  + '  seconds have passed');
+//   if(time > 5){
+//       clearInterval(timer);
+//   }
+// }, 2000);
+
+
+
+
+// // var fs = require('fs');
+
+// // var file = fs.readFile('input.txt','utf8',function(err,data){
+// //     fs.writeFile('writeme.txt',data);
+// //     console.log(data);
+// // });
+
+// // console.log('This is an instruction outside the sync file system.');
